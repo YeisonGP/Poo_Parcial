9d4ab9718ca1af19bfc75b2b9237dcf54ca24eff
YeisonGP <115736572+YeisonGP@users.noreply.github.com>
2022-11-28 15:11:03 -0300

    3
    Contenido Shape.
    estoy usando Eclipse

--- Parcial2/src/Shape.java
+++ Parcial2/src/Shape.java
@@ -1,4 +1,43 @@
 
 public abstract class Shape {
 
+		public String color= "red";
+	    public boolean filled=true;
+	    public double area;
+	    public double perimeter;
+
+	    Shape(){
+	        this.color = "blue";
+	        this.filled = false;
+	        this.area = 0;
+	        this.perimeter = 0;
+	    }
+	    Shape(String color, boolean filled){
+	        this.setColor(color);
+	        this.setFilled(filled);
+	    }
+	    
+		public String getColor() {
+			return color;
+		}
+		public void setColor(String color) {
+			this.color = color;
+		}
+		public boolean isFilled() {
+			return filled;
+		}
+		public void setFilled(boolean filled) {
+			this.filled = filled;
+		}
+		public double getArea() {
+			return area;
+		}
+
+		public double getPerimeter() {
+			return perimeter;
+		}
+
+	    
+	
+	
 }
