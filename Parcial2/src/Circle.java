a7db38abf6d3d412e48a489124b20138fa86b207
YeisonGP <115736572+YeisonGP@users.noreply.github.com>
2022-11-28 15:24:57 -0300

    4
    arregle el extends en circle sin querer coloque Squere y era shape.
    Agregado de las todo el contenido de Circle los calculos de area y perimetro saque de google.

--- Parcial2/src/Circle.java
+++ Parcial2/src/Circle.java
@@ -1,4 +1,46 @@
 
-public class Circle {
+public class Circle extends Shape {
 
+	protected double radius = 1.0;
+	
+	
+	
+    Circle(){
+        this.color = "blue";
+        this.radius = 3.7;
+        this.filled = true;
+        this.area = 43;
+        this.perimeter = 23.23;
+    }
+
+    Circle(double radius) {
+        this.setRadius(radius);
+    }
+
+    Circle(double radius, String color, boolean filled) {
+        super(color, filled);
+        this.setRadius(radius);
+    }
+	
+	
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	public double getArea() {
+		area = (radius*radius)*3.14;
+		return area;
+	}
+
+	@Override
+	public double getPerimeter() {
+		perimeter = 2*Math. PI*radius;
+		return perimeter;
+	}
+		
 }
